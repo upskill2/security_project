6f26d80c8add2263c558569d6a320eb0d82132af
taras.chmeruk <taras.chmeruk@gameloft.com>
2024-09-26 15:12:06 +0300

    final

--- section16-auth-server/src/main/java/com/security/course/section16_auth_server/config/SecurityConfig.java
+++ section16-auth-server/src/main/java/com/security/course/section16_auth_server/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -26,6 +27,10 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -35,6 +40,8 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.util.Set;
 import java.util.UUID;
 
 @Configuration
@@ -43,40 +50,40 @@ public class SecurityConfig {
 
     @Bean
     @Order (1)
-    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
+    public SecurityFilterChain authorizationServerSecurityFilterChain (HttpSecurity http)
             throws Exception {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-                .oidc(Customizer.withDefaults());	// Enable OpenID Connect 1.0
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity (http);
+        http.getConfigurer (OAuth2AuthorizationServerConfigurer.class)
+                .oidc (Customizer.withDefaults ());    // Enable OpenID Connect 1.0
         http
                 // Redirect to the login page when not authenticated from the
                 // authorization endpoint
-                .exceptionHandling((exceptions) -> exceptions
-                        .defaultAuthenticationEntryPointFor(
+                .exceptionHandling ((exceptions) -> exceptions
+                        .defaultAuthenticationEntryPointFor (
                                 new LoginUrlAuthenticationEntryPoint ("/login"),
                                 new MediaTypeRequestMatcher (MediaType.TEXT_HTML)
                         )
                 )
                 // Accept access tokens for User Info and/or Client Registration
-                .oauth2ResourceServer((resourceServer) -> resourceServer
-                        .jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer ((resourceServer) -> resourceServer
+                        .jwt (Customizer.withDefaults ()));
 
-        return http.build();
+        return http.build ();
     }
 
     @Bean
-    @Order(2)
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+    @Order (2)
+    public SecurityFilterChain defaultSecurityFilterChain (HttpSecurity http)
             throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests ((authorize) -> authorize
+                        .anyRequest ().authenticated ()
                 )
                 // Form login handles the redirect to the login page from the
                 // authorization server filter chain
-                .formLogin(Customizer.withDefaults());
+                .formLogin (Customizer.withDefaults ());
 
-        return http.build();
+        return http.build ();
     }
 
 /*    @Bean
@@ -91,58 +98,110 @@ public class SecurityConfig {
     }*/
 
     @Bean
-    public RegisteredClientRepository registeredClientRepository() {
-        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("oidc-client")
-                .clientSecret("{noop}secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
-                .postLogoutRedirectUri("http://127.0.0.1:8080/")
-                .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-                .build();
+    public RegisteredClientRepository registeredClientRepository () {
+        RegisteredClient oidcClient = RegisteredClient.withId (UUID.randomUUID ().toString ())
+                .clientId ("bankapi")
+                .clientSecret ("{noop}secret")
+                .clientAuthenticationMethod (ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType (AuthorizationGrantType.CLIENT_CREDENTIALS)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                //         .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+                //          .postLogoutRedirectUri("http://127.0.0.1:8080/")
+                .scope (OidcScopes.OPENID)
+                .scope ("ADMIN")
+                .scope ("USER")
+                .tokenSettings (TokenSettings.builder ()
+                        .accessTokenTimeToLive (Duration.ofMinutes (100))
+                        .accessTokenFormat (OAuth2TokenFormat.SELF_CONTAINED)
+                        .build ())
+                //.clientSettings (ClientSettings.builder ().requireAuthorizationConsent (true).build ())
+                .build ();
 
-        return new InMemoryRegisteredClientRepository (oidcClient);
+        RegisteredClient authClient = RegisteredClient.withId (UUID.randomUUID ().toString ())
+                .clientId ("bankapiclient")
+                .clientSecret ("{noop}secret")
+                .clientAuthenticationMethod (ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .authorizationGrantType (AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType (AuthorizationGrantType.REFRESH_TOKEN)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri ("https://oauth.pstmn.io/v1/callback")
+                //          .postLogoutRedirectUri("http://127.0.0.1:8080/")
+                .scope (OidcScopes.OPENID)
+                .tokenSettings (TokenSettings.builder ()
+                        .accessTokenTimeToLive (Duration.ofMinutes (100))
+                        .refreshTokenTimeToLive (Duration.ofMinutes (10)).reuseRefreshTokens (false)
+                        .accessTokenFormat (OAuth2TokenFormat.SELF_CONTAINED)
+                        .build ())
+                //.clientSettings (ClientSettings.builder ().requireAuthorizationConsent (true).build ())
+                .build ();
+
+        RegisteredClient pkceClient = RegisteredClient.withId (UUID.randomUUID ().toString ())
+                .clientId ("bankapipkce")
+                .clientSecret ("{noop}secret")
+                .clientAuthenticationMethod (ClientAuthenticationMethod.NONE)
+                .authorizationGrantType (AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType (AuthorizationGrantType.REFRESH_TOKEN)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri ("https://oauth.pstmn.io/v1/callback")
+                //          .postLogoutRedirectUri("http://127.0.0.1:8080/")
+                .scope (OidcScopes.OPENID)
+                .clientSettings (ClientSettings.builder ().requireProofKey (true).build ())
+                .tokenSettings (TokenSettings.builder ()
+                        .accessTokenTimeToLive (Duration.ofMinutes (100))
+                        .accessTokenFormat (OAuth2TokenFormat.SELF_CONTAINED)
+                        .build ())
+                //.clientSettings (ClientSettings.builder ().requireAuthorizationConsent (true).build ())
+                .build ();
+
+        return new InMemoryRegisteredClientRepository (oidcClient, pkceClient, authClient);
     }
 
     @Bean
-    public JWKSource<SecurityContext> jwkSource() {
-        KeyPair keyPair = generateRsaKey();
-        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-        RSAKey rsaKey = new RSAKey.Builder(publicKey)
-                .privateKey(privateKey)
-                .keyID(UUID.randomUUID().toString())
-                .build();
+    public JWKSource<SecurityContext> jwkSource () {
+        KeyPair keyPair = generateRsaKey ();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic ();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate ();
+        RSAKey rsaKey = new RSAKey.Builder (publicKey)
+                .privateKey (privateKey)
+                .keyID (UUID.randomUUID ().toString ())
+                .build ();
         JWKSet jwkSet = new JWKSet (rsaKey);
         return new ImmutableJWKSet<> (jwkSet);
     }
 
-    private static KeyPair generateRsaKey() {
+    private static KeyPair generateRsaKey () {
         KeyPair keyPair;
         try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            keyPairGenerator.initialize(2048);
-            keyPair = keyPairGenerator.generateKeyPair();
-        }
-        catch (Exception ex) {
-            throw new IllegalStateException(ex);
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance ("RSA");
+            keyPairGenerator.initialize (2048);
+            keyPair = keyPairGenerator.generateKeyPair ();
+        } catch (Exception ex) {
+            throw new IllegalStateException (ex);
         }
         return keyPair;
     }
 
     @Bean
-    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
-        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    public JwtDecoder jwtDecoder (JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder (jwkSource);
     }
 
     @Bean
-    public AuthorizationServerSettings authorizationServerSettings() {
-        return AuthorizationServerSettings.builder().build();
+    public AuthorizationServerSettings authorizationServerSettings () {
+        return AuthorizationServerSettings.builder ().build ();
     }
 
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer () {
+        return (context) -> {
+            if (context.getTokenType ().equals (OAuth2TokenType.ACCESS_TOKEN)) {
+                context.getClaims ().claims ((cl) -> {
+                    final Set<String> scope = context.getClaims ().build ().getClaim ("scope");
+                    cl.put ("roles", scope);
+                });
+            }
+        };
+    }
 
 }
